b57ed5bb483a85acbbd422ef8932eaf5ce492286
Parmar Satyam <satyamparmar19@gmail.com>
2026-09-17 04:33:00 +0530

    fix: frontend live api url with path

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,9 +2,9 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://onrender.com",
     withCredentials: true
-})
+});
 
 export async function register({ username, email, password }) {
 

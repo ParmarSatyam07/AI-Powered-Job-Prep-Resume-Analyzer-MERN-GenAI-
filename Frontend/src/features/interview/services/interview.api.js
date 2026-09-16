348df154caf9bfca32d6957a5652b6e5d4a2f95b
Parmar Satyam <satyamparmar19@gmail.com>
2026-09-17 03:14:08 +0530

    Update production backend URL

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://ai-powered-job-prep-resume-analyzer-mern.onrender.com",
     withCredentials: true,
-})
+});
 
 
 /**
